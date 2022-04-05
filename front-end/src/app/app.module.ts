9edf40615aa1beed1e2ac97bc3a80ed300a623b8
unknown <Maherisoa-23>
2022-04-05 09:39:30 +0200

    view list-active-visits

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VisitDashboardComponent } from './visit-dashboard/visit-dashboard.component';
+import { ListActiveVisitComponent } from './visit-dashboard/list-active-visit/list-active-visit.component';
+import { VisitComponent } from './visit-dashboard/list-active-visit/visit/visit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VisitDashboardComponent
+    VisitDashboardComponent,
+    ListActiveVisitComponent,
+    VisitComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/visit-dashboard/list-active-visit/list-active-visit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-active-visit',
+  templateUrl: './list-active-visit.component.html',
+  styleUrls: ['./list-active-visit.component.css']
+})
+export class ListActiveVisitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
