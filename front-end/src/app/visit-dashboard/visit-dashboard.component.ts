2e21a9cda61ef252b489912c10c0eb2bd0c73eaa
unknown <Maherisoa-23>
2022-04-05 09:32:01 +0200

    installation bootstrap

---
+++ front-end/src/app/visit-dashboard/visit-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-dashboard',
+  templateUrl: './visit-dashboard.component.html',
+  styleUrls: ['./visit-dashboard.component.css']
+})
+export class VisitDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
