df3d0b51967faed5688711cfab32012ebe1a5a0e
unknown <Maherisoa-23>
2022-05-06 13:53:17 +0100

    triage par heure d'entrée et par departement opérationnel

--- front-end/src/app/admin-dashboard/pointage-register/pointage-register.component.ts
+++ front-end/src/app/admin-dashboard/pointage-register/pointage-register.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class PointageRegisterComponent implements OnInit {
 
+  dep_croissant = false
+  entry_time_croissant = false
   pointages: any;
   constructor(private authServ: AuthService) { }
 
@@ -19,5 +21,27 @@ export class PointageRegisterComponent implements OnInit {
     this.authServ.getPointageRegisterList().subscribe((data) => {
       this.pointages = data;
     });
+  }
+
+  SortByDep(){
+    if (!this.dep_croissant) {
+      this.pointages.sort((a : any,b : any) => a.employee_dep_name.localeCompare(b.employee_dep_name));
+      this.dep_croissant = true
+    }
+    else {
+      this.pointages.sort((b : any,a : any) => a.employee_dep_name.localeCompare(b.employee_dep_name));
+      this.dep_croissant = false
+    }
+  }
+
+  SortByEntryTime(){
+    if (!this.entry_time_croissant) {
+      this.pointages.sort((a : any,b : any) => a.entry_time.localeCompare(b.entry_time));
+      this.entry_time_croissant = true
+    }
+    else {
+      this.pointages.sort((b : any,a : any) => a.entry_time.localeCompare(b.entry_time));
+      this.entry_time_croissant = false
+    }
   }
 }
