7d4c50dcb0fe0f5619a4074287698040d06d1027
Lucasrakoto <rakotoarihantalucas839@gmail.com>
2022-05-10 15:36:44 -0800

    ajustement delai pour un ordi lent

--- front-end/src/app/admin-dashboard/accueil/chart-generale/chart-generale.component.ts
+++ front-end/src/app/admin-dashboard/accueil/chart-generale/chart-generale.component.ts
@@ -5,7 +5,7 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-chart-generale',
   templateUrl: './chart-generale.component.html',
-  styleUrls: ['./chart-generale.component.css']
+  styleUrls: ['./chart-generale.component.css'],
 })
 export class ChartGeneraleComponent implements OnInit {
   @Input() tab_visit_counting: any = [];
@@ -14,14 +14,13 @@ export class ChartGeneraleComponent implements OnInit {
   myChartVisite: any;
   myChartPointage: any;
 
-  constructor(private elementRef: ElementRef, private authServ: AuthService) {
-  }
+  constructor(private elementRef: ElementRef, private authServ: AuthService) {}
 
   ngOnInit(): void {
-    this.refreshCounting()
+    this.refreshCounting();
     setTimeout(() => {
-      this.chartit()
-    }, 500);
+      this.chartit();
+    }, 1000);
   }
 
   refreshCounting() {
@@ -31,49 +30,68 @@ export class ChartGeneraleComponent implements OnInit {
     this.authServ.getTabPointageCounting().subscribe((data) => {
       this.tab_pointage_counting = data;
     });
-
   }
   chartit() {
     const data_visite = {
-      labels: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
-      datasets: [{
-        label: 'Ambohijatovo',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: '#ca212680',
-        maxBarThickness: 12,
-      },
-      {
-        label: 'Andraharo',
-        data: [15, 32, 75, 94, 40, 66, 35],
-        backgroundColor: '#37a94a80',
-        maxBarThickness: 12,
-      },
-      {
-        label: 'Mangasoavina',
-        data: [48, 34, 16, 74, 53, 26, 68],
-        backgroundColor: '#1e546b80',
-        maxBarThickness: 12,
-      }
-    ]
-    }
+      labels: [
+        'Lundi',
+        'Mardi',
+        'Mercredi',
+        'Jeudi',
+        'Vendredi',
+        'Samedi',
+        'Dimanche',
+      ],
+      datasets: [
+        {
+          label: 'Ambohijatovo',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: '#ca212680',
+          maxBarThickness: 12,
+        },
+        {
+          label: 'Andraharo',
+          data: [15, 32, 75, 94, 40, 66, 35],
+          backgroundColor: '#37a94a80',
+          maxBarThickness: 12,
+        },
+        {
+          label: 'Mangasoavina',
+          data: [48, 34, 16, 74, 53, 26, 68],
+          backgroundColor: '#1e546b80',
+          maxBarThickness: 12,
+        },
+      ],
+    };
     const data_pointage = {
-      labels: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        borderColor: '#37ab4b',
-      }]
-    }
-    let htmlRefVisit = this.elementRef.nativeElement.querySelector(`#myChartVisite`);
+      labels: [
+        'Lundi',
+        'Mardi',
+        'Mercredi',
+        'Jeudi',
+        'Vendredi',
+        'Samedi',
+        'Dimanche',
+      ],
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          borderColor: '#37ab4b',
+        },
+      ],
+    };
+    let htmlRefVisit =
+      this.elementRef.nativeElement.querySelector(`#myChartVisite`);
     this.myChartVisite = new Chart(htmlRefVisit, {
       type: 'bar',
       data: data_visite,
     });
-    let htmlRefPointage = this.elementRef.nativeElement.querySelector(`#myChartPointage`);
+    let htmlRefPointage =
+      this.elementRef.nativeElement.querySelector(`#myChartPointage`);
     this.myChartPointage = new Chart(htmlRefPointage, {
       type: 'line',
       data: data_pointage,
     });
-
   }
 }
--- front-end/src/app/admin-dashboard/accueil/lieu-card/lieu-card.component.ts
+++ front-end/src/app/admin-dashboard/accueil/lieu-card/lieu-card.component.ts
@@ -5,21 +5,20 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-lieu-card',
   templateUrl: './lieu-card.component.html',
-  styleUrls: ['./lieu-card.component.css']
+  styleUrls: ['./lieu-card.component.css'],
 })
 export class LieuCardComponent implements OnInit {
-  @Input() lieu = ""
-  @Input() employee_name = ""
-  @Input() entry_time = ""
-  nb_visite : any
-  nb_pointage : any
-  nb_employee : any
+  @Input() lieu = '';
+  @Input() employee_name = '';
+  @Input() entry_time = '';
+  nb_visite: any;
+  nb_pointage: any;
+  nb_employee: any;
   myChart: any;
   id_lieu = 0;
-  couleur = ""
+  couleur = '';
 
-
-  constructor(private elementRef: ElementRef, private authServ: AuthService) { }
+  constructor(private elementRef: ElementRef, private authServ: AuthService) {}
 
   ngOnInit(): void {
     this.id_lieu = this.getLieuId(this.lieu);
@@ -27,52 +26,56 @@ export class LieuCardComponent implements OnInit {
     this.refreshCounting();
 
     setTimeout(() => {
-      this.chartit()}, 500); 
+      this.chartit();
+    }, 1700);
   }
 
   getLieuId(lieu: string) {
-    if (lieu == "Ambohijatovo") return 1
+    if (lieu == 'Ambohijatovo') return 1;
     else {
-      if (lieu == "Andraharo") return 2
-      else return 3
+      if (lieu == 'Andraharo') return 2;
+      else return 3;
     }
   }
 
-  setColor(id_lieu : number){
-    if (id_lieu == 1) this.couleur = "#c92128"
+  setColor(id_lieu: number) {
+    if (id_lieu == 1) this.couleur = '#c92128';
     else {
-      if (id_lieu == 2) this.couleur = "#37ab4b"
-      else this.couleur = "#20566d"
-    } 
+      if (id_lieu == 2) this.couleur = '#37ab4b';
+      else this.couleur = '#20566d';
+    }
   }
 
   refreshCounting() {
-    const l = this.lieu
-    this.authServ.getVisitCountingFilterByPlace(this.id_lieu).subscribe((data) => {
-      this.nb_visite = data;
-    });
-    this.authServ.getPointageCountingFilterByPlace(this.id_lieu).subscribe((data) => {
-      this.nb_pointage = data;
-    });
-    this.authServ.getTotalNumberOfEmployeeByPlace(this.id_lieu).subscribe((data) => {
-      this.nb_employee = data;
-    });
+    const l = this.lieu;
+    this.authServ
+      .getVisitCountingFilterByPlace(this.id_lieu)
+      .subscribe((data) => {
+        this.nb_visite = data;
+      });
+    this.authServ
+      .getPointageCountingFilterByPlace(this.id_lieu)
+      .subscribe((data) => {
+        this.nb_pointage = data;
+      });
+    this.authServ
+      .getTotalNumberOfEmployeeByPlace(this.id_lieu)
+      .subscribe((data) => {
+        this.nb_employee = data;
+      });
   }
 
   chartit() {
     const data = {
-      labels: [
-        'pourcentage des employés',
+      labels: ['pourcentage des employés'],
+      datasets: [
+        {
+          label: 'My First Dataset',
+          data: [this.nb_pointage, this.nb_employee - this.nb_pointage],
+          backgroundColor: [this.couleur, '#eeeeee'],
+          hoverOffset: 4,
+        },
       ],
-      datasets: [{
-        label: 'My First Dataset',
-        data: [this.nb_pointage, this.nb_employee - this.nb_pointage],
-        backgroundColor: [
-          this.couleur,
-          '#eeeeee',
-        ],
-        hoverOffset: 4
-      }]
     };
     let htmlRef = this.elementRef.nativeElement.querySelector(`#lieu-chart`);
     this.myChart = new Chart(htmlRef, {
@@ -80,9 +83,9 @@ export class LieuCardComponent implements OnInit {
       data: data,
       options: {
         animation: {
-          animateScale : true
-        }
-      }
+          animateScale: true,
+        },
+      },
     });
   }
 }
