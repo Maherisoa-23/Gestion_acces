6a9630d76ae0a1020dcdbdc85b0c3a5a89e554b6
unknown <Maherisoa-23>
2022-05-17 10:15:02 +0100

    widget de la date opérationnel 2.0

--- front-end/src/app/admin-dashboard/pointage-register/pointage-register.component.ts
+++ front-end/src/app/admin-dashboard/pointage-register/pointage-register.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy , ViewChild } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { DataTableDirective } from 'angular-datatables' ;
 declare var $: any;
 
 @Component({
@@ -25,10 +26,15 @@ export class PointageRegisterComponent implements OnInit {
   departments: any;
   DateSelected: any;
 
-  filtredPointages: any = [];
-
   dtOptions: DataTables.Settings = {};
   isShow = false
+  table : any;
+
+  //filtrage personnalisé
+  @ViewChild ( DataTableDirective , { static : false }) 
+  datatableElement : any = DataTableDirective ;  
+
+  depSelected : any;
   constructor(private authServ: AuthService) {}
 
   ngOnInit(): void {
@@ -41,19 +47,14 @@ export class PointageRegisterComponent implements OnInit {
         pagingType: 'full_numbers',
         pageLength: 5,
         lengthMenu : [5, 10, 25],
-        processing: true
+        processing: true,
       };
     },500)
-    
     $('.dateadded').on('change', function (ret: any) {
       var v = ret.target.value; // getting search input value
 
       $('#dataTables-example').DataTable().columns(1).search(v).draw();
-    });
-
-    $(".namefilter").on("change", function(){
-      $('#dataTables-example').DataTable().order([0, $().val()]).draw();
-    });
+    });    
   }
 
   refreshPointageRegisterList() {
@@ -68,172 +69,6 @@ export class PointageRegisterComponent implements OnInit {
     });
   }
 
-  /* Les méthodes de triage */
-  SortByDep() {
-    this.trie_dep = true;
-    this.trie_date =
-      this.trie_entry_time =
-      this.trie_exit_time =
-      this.trie_nom =
-      this.trie_lieu =
-        false;
-    if (!this.dep_croissant) {
-      this.pointages.sort((a: any, b: any) =>
-        a.employee_dep_name.localeCompare(b.employee_dep_name)
-      );
-      this.dep_croissant = true;
-    } else {
-      this.pointages.sort((b: any, a: any) =>
-        a.employee_dep_name.localeCompare(b.employee_dep_name)
-      );
-      this.dep_croissant = false;
-    }
-  }
-
-  SortByEntryTime() {
-    this.trie_entry_time = true;
-    this.trie_date =
-      this.trie_dep =
-      this.trie_exit_time =
-      this.trie_nom =
-      this.trie_lieu =
-        false;
-    if (!this.entry_time_croissant) {
-      this.pointages.sort((a: any, b: any) =>
-        a.entry_time.localeCompare(b.entry_time)
-      );
-      this.entry_time_croissant = true;
-    } else {
-      this.pointages.sort((b: any, a: any) =>
-        a.entry_time.localeCompare(b.entry_time)
-      );
-      this.entry_time_croissant = false;
-    }
-  }
-
-  SortByExitTime() {
-    this.trie_exit_time = true;
-    this.trie_date =
-      this.trie_dep =
-      this.trie_entry_time =
-      this.trie_nom =
-      this.trie_lieu =
-        false;
-    if (!this.exit_time_croissant) {
-      this.pointages.sort((a: any, b: any) =>
-        a.exit_time.localeCompare(b.exit_time)
-      );
-      this.exit_time_croissant = true;
-    } else {
-      this.pointages.sort((b: any, a: any) =>
-        a.exit_time.localeCompare(b.exit_time)
-      );
-      this.exit_time_croissant = false;
-    }
-  }
-
-  SortByDate() {
-    this.trie_date = true;
-    this.trie_exit_time =
-      this.trie_dep =
-      this.trie_entry_time =
-      this.trie_nom =
-      this.trie_lieu =
-        false;
-    if (!this.date_croissant) {
-      this.pointages.sort((a: any, b: any) => a.date.localeCompare(b.date));
-      this.date_croissant = true;
-    } else {
-      this.pointages.sort((b: any, a: any) => a.date.localeCompare(b.date));
-      this.date_croissant = false;
-    }
-  }
-
-  SortByName() {
-    this.trie_nom = true;
-    this.trie_exit_time =
-      this.trie_dep =
-      this.trie_entry_time =
-      this.trie_date =
-      this.trie_lieu =
-        false;
-    if (!this.nom_croissant) {
-      this.pointages.sort((a: any, b: any) =>
-        a.employee_name.localeCompare(b.employee_name)
-      );
-      this.nom_croissant = true;
-    } else {
-      this.pointages.sort((b: any, a: any) =>
-        a.employee_name.localeCompare(b.employee_name)
-      );
-      this.nom_croissant = false;
-    }
-  }
-  SortByLieu() {
-    this.trie_lieu = true;
-    this.trie_date =
-      this.trie_entry_time =
-      this.trie_exit_time =
-      this.trie_nom =
-      this.trie_dep =
-        false;
-    if (!this.lieu_croissant) {
-      this.pointages.sort((a: any, b: any) => a.lieu.localeCompare(b.lieu));
-      this.lieu_croissant = true;
-    } else {
-      this.pointages.sort((b: any, a: any) => a.lieu.localeCompare(b.lieu));
-      this.lieu_croissant = false;
-    }
-  }
-
-  //Methode fe filtrage
-  @Input() selected_lieu = '';
-  @Input() selected_dep = '';
-  filtered = false;
-  FilterByLieu(lieu_name: string) {
-    this.filtered = true;
-    this.filtredPointages = [];
-    for (let index = 0; index < this.pointages.length; index++) {
-      const element = this.pointages[index];
-      console.log(element.employee_dep_name);
-      if (element.lieu == lieu_name) {
-        this.filtredPointages.push(element);
-      }
-    }
-    this.SortByName();
-  }
-
-  FilterByDep() {
-    this.filtered = true;
-    this.filtredPointages = [];
-    for (let index = 0; index < this.pointages.length; index++) {
-      const element = this.pointages[index];
-      if (element.employee_dep_name == this.selected_dep) {
-        this.filtredPointages.push(element);
-      }
-    }
-    this.SortByName();
-  }
-
-  DepSelected(item: any) {
-    this.selected_dep = item.department_name;
-    setTimeout(() => {
-      this.FilterByDep();
-    }, 500);
-  }
-
-  AmbohijatovoSelected() {
-    this.selected_lieu = 'Ambohijatovo';
-    this.FilterByLieu(this.selected_lieu);
-  }
-  AndraharoSelected() {
-    this.selected_lieu = 'Andraharo';
-    this.FilterByLieu(this.selected_lieu);
-  }
-  MangasoavinaSelected() {
-    this.selected_lieu = 'Mangasoavina';
-    this.FilterByLieu(this.selected_lieu);
-  }
 
   // ChangeDate(DateSelected: Date) {
   //   if(DateSelected == item.date){
