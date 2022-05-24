536247292593cbb11f5ff310a42fea54b608eb91
Lucasrakoto <rakotoarihantalucas839@gmail.com>
2022-05-24 13:09:32 -0800

    teEs

---
+++ front-end/src/app/admin-dashboard/employee/employee.component.ts
@@ -0,0 +1,115 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+})
+export class EmployeeComponent implements OnInit {
+  employees: any;
+  pointage_register: any;
+  pointages: any;
+  last_pointage: any;
+  trie_nom = false;
+  trie_matricule = false;
+  nom_croissant = false;
+  matricule_croissant = false;
+  departments: any;
+
+  constructor(
+    private authServ: AuthService,
+    // private SecurityServ: SecurityAgentService,
+    private route: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.refreshPointageRegisterList();
+    this.refreshDepList();
+    setTimeout(() => {
+      this.refreshSecurityList();
+    }, 500);
+  }
+  refreshDepList() {
+    this.authServ.getDepartmentList().subscribe((data) => {
+      this.departments = data;
+    });
+  }
+
+  refreshSecurityList() {
+    this.authServ.getEmployeeList().subscribe((data) => {
+      this.employees = data;
+    });
+  }
+
+  refreshPointageList() {
+    this.authServ.getPointageList().subscribe((data) => {
+      this.pointages = data;
+    });
+  }
+
+  refreshPointageRegisterList() {
+    this.authServ.getPointageRegisterList().subscribe((data) => {
+      this.pointage_register = data;
+    });
+  }
+
+  getLastPointageBySec(matricule: number) {
+    for (let index = 0; index < this.pointage_register.length; index++) {
+      const element = this.pointage_register[index];
+      if (element.numero_matricule == matricule) {
+        this.last_pointage = element;
+      }
+    }
+    return (
+      'le ' +
+      this.last_pointage.date +
+      ' ' +
+      this.last_pointage.exit_time +
+      ' à ' +
+      this.last_pointage.lieu
+    );
+  }
+
+  SortByName() {
+    this.trie_nom = true;
+    this.trie_matricule = false;
+    if (!this.nom_croissant) {
+      this.employees.sort((a: any, b: any) =>
+        a.employee_name.localeCompare(b.employee_name)
+      );
+      this.nom_croissant = true;
+    } else {
+      this.employees.sort((b: any, a: any) =>
+        a.employee_name.localeCompare(b.employee_name)
+      );
+      this.nom_croissant = false;
+    }
+  }
+
+  SortByMatricule() {
+    this.trie_matricule = true;
+    this.trie_nom = false;
+    if (!this.matricule_croissant) {
+      this.employees.sort(
+        (a: any, b: any) => a.numero_matricule - b.numero_matricule
+      );
+      this.matricule_croissant = true;
+    } else {
+      this.employees.sort(
+        (b: any, a: any) => a.numero_matricule - b.numero_matricule
+      );
+      this.matricule_croissant = false;
+    }
+  }
+
+  // ShowSecurityProfile(security: any) {
+  //   this.authServ.refreshPointageList();
+  //   setTimeout(() => {
+  //     this.SecurityServ.matricule_security = security.numero_matricule;
+  //     this.SecurityServ.security_name = security.employee_name;
+  //     this.SecurityServ.pointed_at = security.pointed_at;
+  //     this.route.navigate(['admin/security-profile']);
+  //   }, 1000);
+  // }
+}
--- front-end/src/app/services/auth.service.ts
+++ front-end/src/app/services/auth.service.ts
@@ -1,30 +1,27 @@
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class AuthService {
   isAuth = false;
-  userName = "";
-  lieu = "Ambohijatovo";
+  userName = '';
+  lieu = 'Ambohijatovo';
   isAdmin = false;
-  date : any;
+  date: any;
 
-  pointages:any;
+  pointages: any;
 
   //pour passer des données entre les composants.
-  dateDailyPointage = ""
-  numero_matricule = 0
+  dateDailyPointage = '';
+  numero_matricule = 0;
 
-  readonly APIUrl = "http://192.168.1.23:8000"
-  //readonly APIUrl = "http://localhost:8000"
-  
+  // readonly APIUrl = "http://192.168.1.23:8000"
+  readonly APIUrl = 'http://localhost:8000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   signIn() {
     return new Promise((resolve, reject) => {
@@ -49,12 +46,12 @@ export class AuthService {
     return this.http.get<any[]>(this.APIUrl + '/employee/');
   }
 
-  getEmployeeListByDep(dep_id :number): Observable<any[]> {
+  getEmployeeListByDep(dep_id: number): Observable<any[]> {
     return this.http.delete<any[]>(this.APIUrl + '/employee/' + dep_id);
   }
 
-  putEmployee(val : any){
-    return this.http.put(this.APIUrl + '/employee/',val);
+  putEmployee(val: any) {
+    return this.http.put(this.APIUrl + '/employee/', val);
   }
 
   getDepartmentList(): Observable<any[]> {
@@ -65,8 +62,8 @@ export class AuthService {
     return this.http.get<any[]>(this.APIUrl + '/lieu/');
   }
 
-  putLieu (val: any) {
-    return this.http.put(this.APIUrl + '/lieu/',val);
+  putLieu(val: any) {
+    return this.http.put(this.APIUrl + '/lieu/', val);
   }
 
   //Methode pour les pointages
@@ -86,47 +83,45 @@ export class AuthService {
   }
 
   addPointage(val: any) {
-    return this.http.post(this.APIUrl + '/pointage/',val);
+    return this.http.post(this.APIUrl + '/pointage/', val);
   }
 
   addPointageRegister(val: any) {
-    return this.http.post(this.APIUrl + '/pointage_register/',val);
+    return this.http.post(this.APIUrl + '/pointage_register/', val);
   }
 
   deletePointage(id: number) {
     return this.http.delete(this.APIUrl + '/pointage/' + id);
   }
   //filtre des pointages par départements
-  getPointageByDep(dep_id: number){
+  getPointageByDep(dep_id: number) {
     return this.http.delete(this.APIUrl + '/security/' + dep_id);
   }
 
-
   //methode pour les comptages
   //pour les visites
-  getTabVisitCounting(): Observable<any[]>{
+  getTabVisitCounting(): Observable<any[]> {
     return this.http.get<any[]>(this.APIUrl + '/visit_counter/');
   }
-  getVisitCountingFilterByPlace(lieu_id: number): Observable<any>{
+  getVisitCountingFilterByPlace(lieu_id: number): Observable<any> {
     return this.http.delete<any>(this.APIUrl + '/visit_counter/' + lieu_id);
   }
   //pour les pointages
-  getAllPointageByLieuAndDate(val : any){
+  getAllPointageByLieuAndDate(val: any) {
     return this.http.post(this.APIUrl + '/pointage_counter/', val);
   }
-  getPointageByLieuAndDate(val : any){
+  getPointageByLieuAndDate(val: any) {
     return this.http.post(this.APIUrl + '/daily_pointage/', val);
   }
-  getPointageCountingFilterByPlace(lieu_id: number): Observable<any>{
+  getPointageCountingFilterByPlace(lieu_id: number): Observable<any> {
     return this.http.delete<any>(this.APIUrl + '/pointage_counter/' + lieu_id);
   }
-  getLieu(lieu_id : number): Observable<any>{
+  getLieu(lieu_id: number): Observable<any> {
     return this.http.delete<any>(this.APIUrl + '/lieu/' + lieu_id);
   }
 
-
   //Pour les sécurité actifs par lieu
-  getActifSecurityByLieu(val : any): Observable<any[]>{
+  getActifSecurityByLieu(val: any): Observable<any[]> {
     return this.http.post<any[]>(this.APIUrl + '/security/', val);
   }
 }
